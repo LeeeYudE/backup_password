da9ef67dc9ca69582f2a051099726a18a86eb941
charcolee <634182772@QQ.COM>
2022-08-03 16:46:32 +0700

    修改ci文件 [build]。

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 
 # 打包显示的日志
 
-_tag=v1.0.2
+_tag=v1.0.3
 
-#git tag ${_tag}
-#git push --tags
+git tag ${_tag}
+git push --tags
 git commit --allow-empty -m "[build] Release ${_tag}"
 git push origin $branchg
